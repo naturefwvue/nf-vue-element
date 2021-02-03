0b8afd6483f9130c3b2596f71b8cee2702dff94a
jyk0011 <jyk0013@163.com>
2021-02-03 20:42:58 +0800

    安装element

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(ElementPlus, { size: 'small' })
+  .mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button type="primary" round>主要按钮</el-button>
   </div>
 </template>
